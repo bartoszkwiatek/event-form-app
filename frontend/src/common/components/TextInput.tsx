9804b0a37973c2f65da718737662519db0def93d
Bartosz Kwiatek <bkwiatek3@gmail.com>
2022-09-21 00:45:11 +0200

    feat(frontend): add styles for EventForm

--- frontend/src/common/components/TextInput.tsx
+++ frontend/src/common/components/TextInput.tsx
@@ -1,18 +1,14 @@
 import { FieldHookConfig, useField } from 'formik';
+import './TextInput.scss';
 
 export const TextInput = (
     props: FieldHookConfig<string> & { label: string; 'data-testid': string },
 ) => {
     const [field, meta] = useField(props);
     return (
-        <div data-testid={props['data-testid']}>
+        <div className="text-input" data-testid={props['data-testid']}>
             <label htmlFor={props.name}>{props.label}</label>
-            <input
-                className="text-input"
-                {...field}
-                placeholder={props.placeholder}
-                type={props.type}
-            />
+            <input className="input" {...field} placeholder={props.placeholder} type={props.type} />
             {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
         </div>
     );
